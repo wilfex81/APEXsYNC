97252546461609190bc5d32d15a331ce22c6f339
wilfex <wilfex81@gmail.com>
2026-08-03 12:17:44 -0400

    feat: create transaction staging model

---
+++ data_pipeline/dbt_project/models/stg_transactions.sql
@@ -0,0 +1,24 @@
+with sources as (
+
+    select * from  {{ source('core', 'core_transaction') }}
+
+),
+
+renamed as (
+
+    select 
+        id              as transaction_id,
+        account_id,
+        category_id,
+        date            as transaction_date,
+        amount,
+        description,
+        source_doc_ref,
+        is_duplicate_flag,
+        confidence_score,
+        created_at
+    from source
+
+)
+
+select * from renamed
